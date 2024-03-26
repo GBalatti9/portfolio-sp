bd6fad5681b5b710ef633ad5b236e8e06de4118c
GBalatti9 <gas.balatti@gmail.com>
2024-03-26 09:27:40 -0300

    React router dom configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { AppRouter } from './routes';
+import { BrowserRouter } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   </React.StrictMode>,
 )
