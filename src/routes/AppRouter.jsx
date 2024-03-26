b4fe4a4f7ad2a6d438000d34f4a1793630b9eff9
GBalatti9 <gas.balatti@gmail.com>
2024-03-26 09:31:46 -0300

    Admin view and route configuration

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,5 +1,7 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import { HomePage } from "../main/pages";
+import { LoginPage } from "../auth/pages";
+import { AdminPage } from "../admin/pages/AdminPage";
 
 
 export const AppRouter = () => {
@@ -8,6 +10,8 @@ export const AppRouter = () => {
 
             <Route path="/" element={ <HomePage /> }/>
 
+            <Route path="/login" element={ <LoginPage /> }/>
+            <Route path="/admin" element={ <AdminPage /> }/>
         </Routes>
     )
 }
