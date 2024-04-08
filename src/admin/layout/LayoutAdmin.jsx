38d30b507bc2c52627650b2db4e895d97039152c
GBalatti9 <gas.balatti@gmail.com>
2024-04-08 11:07:09 -0300

    Admin layout

---
+++ src/admin/layout/LayoutAdmin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const LayoutAdmin = ({ children }) => {
+    return (
+        <div className='flex items-center justify-center min-h-screen bg-gray-200'>
+            { children }
+        </div>
+    )
+}
--- src/admin/pages/AdminPage.jsx
+++ src/admin/pages/AdminPage.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { UserContext } from "../../auth/context";
 import { useNavigate } from "react-router-dom";
+import { LayoutAdmin } from "../layout";
 
 
 export const AdminPage = () => {
@@ -15,8 +16,10 @@ export const AdminPage = () => {
 
     return (
         <>
+        <LayoutAdmin>
             <div>Admin Page</div>
             <button onClick={ handleLogout }>Logout</button>
+        </LayoutAdmin>
         </>
     )
 }
