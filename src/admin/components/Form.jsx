ca504e5a63f319c820d18c1285eeb73ea0e65dc0
GBalatti9 <gas.balatti@gmail.com>
2024-04-09 10:22:27 -0300

    Styling form

--- src/admin/components/Form.jsx
+++ src/admin/components/Form.jsx
@@ -4,22 +4,25 @@ import { Inputs } from "./Inputs"
 const formElements = [
     { name: 'Nombre',      type: 'text' },
     { name: 'Descripción', type: 'textarea' },
-    { name: 'Visibilidad', type: 'checkbox' },
     { name: 'Imágenes',    type: 'file' },
     { name: 'Videos',      type: 'file' },
+    { name: 'Visibilidad', type: 'checkbox' },
 ]
 
 export const Form = () => {
     return (
         <>
-            <form className="border border-white bg-white p-4 rounded-md">
+            <form className="border border-white bg-white p-4 rounded-md shadow-lg mb-2 w-8/12">
+                <div className="grid grid-cols-2">
                 {
                     formElements.map(( element, i ) => (
                         <Inputs {...element} key={ element + i } />
-                    ))
+                        ))
                 }
-                <div className="flex justify-end p-4">
-                <Button color={ "bg-blue-400" } text="Guardar" />
+                </div>
+                <div className="flex justify-around p-4">
+                    <Button color={ "bg-red-400" } text="Descartar" />
+                    <Button color={ "bg-blue-400" } text="Guardar" />
                 </div>
             </form>
         </>
--- src/admin/components/Inputs.jsx
+++ src/admin/components/Inputs.jsx
@@ -1,22 +1,36 @@
 
 
 export const Inputs = ({ name, type }) => {
-    console.log( name );
     return (
-        <div className="flex flex-col">
+        <>
             {
-                name === 'Nombre' || name === 'Descripción'
-                ? <label htmlFor={name}>{name}</label>
-                : <label htmlFor={name} className="text-center mt-2">{name}</label>
+                name === 'Descripción' ?
+                    <>
+                        <label htmlFor={name} className="col-span-2 mb-1 font-medium">{name}</label>
+                        <textarea type="text" className="border col-span-2 rounded-md p-2" cols="30" rows="5" id={name} name={name} />
+                    </>
+                    :
+                    name === 'Visibilidad' ?
+                        <>
+                            <div>
+                                <label htmlFor={name} className="col-span-1 mr-2 font-medium">{name}</label>
+                                <input type={type} className="col-span-1 rounded-md p-2" checked />
+                            </div>
+                        </>
+                        :
+                        name === 'Nombre' ?
+                            <>
+                                <label htmlFor={name} className="font-medium">{name}</label>
+                                <input type={type} className="border col-span-2 rounded-md p-2" />
+                            </>
+                            :
+                            <div className="mt-2 flex items-center">
+                                <label htmlFor={name} className="font-medium">{name}</label>
+                                <img src="./icons8-subir-50.png" alt="Cargar archivos" className="cursor-pointer w-1/12 transition-transform duration-300 hover:scale-110"
+                                />
+                                <input type={type} className="rounded-md p-2" multiple />
+                            </div>
             }
-            {
-                type === 'textarea' 
-                ? <textarea name={name} id={name} cols="30" rows="5" className="border rounded-md px-2 py-1 w-full"></textarea>
-                : type === 'text' 
-                ? <input type={type} className="w-full border rounded-md px-2 py-1"/>
-                : <input type={type} className="border w-full m-auto flex justify-center items-center p-2"/>
-                
-            }
-        </div>
+        </>
     )
 }
