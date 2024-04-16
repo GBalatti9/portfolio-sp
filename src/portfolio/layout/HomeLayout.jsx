7ad4f01b3273d4de7afa1b602cdd655b2d0c7ce5
GBalatti9 <gas.balatti@gmail.com>
2024-04-15 21:05:57 -0300

    trying transitions

--- src/portfolio/layout/HomeLayout.jsx
+++ src/portfolio/layout/HomeLayout.jsx
@@ -6,10 +6,10 @@ export const HomeLayout = ({ children, menuVisible }) => {
     return (
         <>
             <Layout>
-                <AnimatePresence mode={'wait'}>
+                <AnimatePresence mode={'wait'} key="home">
                     <motion.div key="home" initial={{ opacity: 0.2 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.75, ease: 'easeInOut' }}>
                         <div className={`${menuVisible ? 'translate-y-20' : ''} transition-transform duration-500 ease-out absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2`}>
-                            {children}
+                            { children }
                         </div>
                         <div className={`absolute top-0 left-0 right-0 transition-transform duration-500 ease-out transform ${menuVisible ? 'translate-y-0' : 'translate-y-[-60px]'}`}>
                             <NavBar />
--- src/portfolio/pages/AboutPage.jsx
+++ src/portfolio/pages/AboutPage.jsx
@@ -1,14 +1,16 @@
 
+import { useState } from "react";
 import { NavBar } from "../../ui/components";
 import { Layout } from "../layout";
 import { motion, AnimatePresence } from 'framer-motion';
 
 
 export const AboutPage = () => {
+
     return (
         <Layout>
-            <AnimatePresence mode={'wait'}>
-                <motion.div key="about" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.75, ease: 'easeInOut' }}>
+            <AnimatePresence mode={'wait'} key="about">
+                <motion.div key="about" initial={{ opacity: 0.4 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 1, ease: 'easeInOut' }}>
                     <NavBar />
                     <div>About page</div>
                 </motion.div>
