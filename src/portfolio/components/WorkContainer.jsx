b246678c5ca53fda7b98fa1d32addc53939aa241
GBalatti9 <gas.balatti@gmail.com>
2024-04-30 14:20:53 -0300

    name each card

--- src/portfolio/components/WorkContainer.jsx
+++ src/portfolio/components/WorkContainer.jsx
@@ -8,12 +8,13 @@ import { LoadingSpinner } from "../../ui/components";
 export const WorkContainer = ({ type }) => {
 
     const [ hoverIndex, setHoverIndex ] = useState(null);
+    const [ underline, setUnderline ]   = useState(null);
     const { projects, isLoading: loading } = useSelector((state) => state.projects);
     const ads = filterDocuments(projects, type);
 
     return (
         <LoadingSpinner loading={loading}>
-            <div className="sm:w-11/12 md:w-8/12 mx-auto grid sm:grid-cols-3 mt-8">
+            <div className="sm:w-11/12 gap-10 mx-auto grid sm:grid-cols-3 mt-8">
                 {
                     ads.length > 0 &&
                     ads?.map((doc, index) => (
@@ -25,6 +26,7 @@ export const WorkContainer = ({ type }) => {
                             key={doc.id}
                         >
                             <WorkCard {...doc} />
+                            <p className={`mt-2 text-blue-800 text-xs text-center sm:text-left`}>{ doc.name }</p>
                             {
                                 hoverIndex === index &&
                                 <div className='transition-opacity duration-300 absolute top-0 w-full h-full flex justify-center items-center bg-gray-950/70 text-white'>
